86777b91e441a089b0379d6699c2da514d5151b0
kazarag <108038002+kazarag@users.noreply.github.com>
2023-07-24 18:47:34 +0700

    Add files via upload

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Camera mainCam;
+
+    private Vector3 mousePos;
+
+    private Rigidbody2D rb;
+
+    public float force;
+
+    public GameObject explosionPrefab;
+
+    public float time;
+
+    public float destroyDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos ;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        Invoke("Explode", time);
+        
+    }
+
+    void Explode()
+    {
+        // Instantiate asset nổ tại vị trí và góc quay của viên đạn
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(explosion, destroyDelay);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")||collision.gameObject.CompareTag("Wall"))
+        {
+            Explode();
+            
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
